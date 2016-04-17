eb73f6f59771ee572489481f2156ac3945687588
Jamaine01 <jroseborough01@gmail.com>
2016-04-17 15:15:12 -0400

    Revision

--- Assignments/A5.py
+++ Assignments/A5.py
@@ -1,4 +1,5 @@
 largest = None
+smallest = None
 while True:
 	user = (raw_input("Enter Number, (enter 'done' to exit):"))
 	if user == "done":
@@ -13,7 +14,14 @@ while True:
 		largest = user
 	elif user > largest:
 		largest = user
+	elif smallest == None:
+		smallest = user
+	elif user < smallest:
+		smallest = user
+	else:
+		continue
 	continue
 print "Done!"
 print "Maximum is " , largest
+print "Minimum is" , smallest
 #without user = int(user) on line 11, accepts as string, makes 67 > 600
