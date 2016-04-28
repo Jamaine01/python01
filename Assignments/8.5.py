b9eec9472f34ddb3961147edf8e2d18a0e7bcb9e
Jamaine01 <jroseborough01@gmail.com>
2016-04-28 15:36:20 -0400

    revised 8.5

---
+++ Assignments/8.5.py
@@ -0,0 +1,15 @@
+fname = raw_input('Enter file name:')
+fh = open(fname)
+count = 0
+name_count = []
+
+for line in fh:
+	if line.startswith('From'):
+		names = line.split()
+		print names[1]
+		count = count + 1
+		name_count.append(names[1])
+
+print 'There were', count, 'lines in the file with from as the first word'
+
+print Counter(name_count)
