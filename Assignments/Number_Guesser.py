5227a544952fe0f6d9835e78e4c0e0a731ea1e7b
Jamaine01 <jroseborough01@gmail.com>
2016-05-13 01:14:23 -0400

    Assignment 11

--- Assignments/Number_Guesser.py
+++ Assignments/Number_Guesser.py
@@ -1,15 +1,23 @@
 import time
 from random import randint
 
-usr = int(raw_input('Enter Secret Number (0-100)!: '))
-print 'Guessing Number...'
-time.sleep(3)
-num_1 = randint(0,100)
-permanent_high = -1
-permanent_low = -1
-count = 1
+while True:
+	usr = int(raw_input('Enter Secret Number (0-100)!: '))
+	try:
+		usr < 101 and usr > -1
+	except:
+		print 'Please enter valid value. Try again.'
+		continue
 
-print num_1 ,'\n' ,'Is this number correct?'
+	print 'Guessing Number...'
+	time.sleep(3)
+	num_1 = randint(0,100)
+	permanent_high = -1
+	permanent_low = -1
+	count = 1
+
+	print num_1 ,'\n' ,'Is this number correct?'
+	break
 
 while True:
 	if permanent_high - permanent_low == 2:
@@ -17,11 +25,21 @@ while True:
 		break
 	else:
 		usr_ans = raw_input('Yes or No?:').lower()
+		try:
+			usr_ans == 'yes' or 'no'
+		except:
+			print 'Not a valid answer. Try again.'
+			continue
 		if usr_ans == 'yes':
 			break
 		elif usr_ans == 'no':
 			count = count + 1
 			hint = raw_input('Is your number higher or lower?:')
+			try:
+				hint == 'higher' or 'lower'
+			except:
+				print 'Not a valid answer. Try again.'
+				continue
 			if hint == 'higher':
 				if permanent_low == -1:
 					permanent_low = num_1
